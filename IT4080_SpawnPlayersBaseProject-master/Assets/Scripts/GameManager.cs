485d502fb14630eb791a4fe1731920846b44ad1e
Lucas Thielmann <thielmlw@mail.uc.edu>
2022-11-07 19:55:28 -0500

    added player spawn

--- IT4080_SpawnPlayersBaseProject-master/Assets/Scripts/GameManager.cs
+++ IT4080_SpawnPlayersBaseProject-master/Assets/Scripts/GameManager.cs
@@ -4,5 +4,58 @@ using UnityEditor;
 using UnityEngine;
 
 public class GameManager : NetworkBehaviour {
+    public Player playerPrefab;
+    public GameObject spawnPoints;
 
+    private int spawnIndex = 0;
+    private List<Transform> availableSpawnPositions = new List<Transform>();
+
+    public void Awake()
+    {
+        refreshSpawnPoints();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsHost)
+        {
+            SpawnPlayers();
+        }
+    }
+
+    private void refreshSpawnPoints()
+    {
+        Transform[] allpoints = spawnPoints.GetComponentsInChildren<Transform>();
+        availableSpawnPositions.Clear();
+        foreach (Transform trans in allpoints)
+        {
+            if (trans != spawnPoints.transform)
+            {
+                availableSpawnPositions.Add(trans);
+            }
+        }
+    }
+
+    public Vector3 GetNextSpawnLocation()
+    {
+        var newPosition = availableSpawnPositions[spawnIndex].position;
+        newPosition.y = 1.5f;
+        spawnIndex += 1;
+
+        if (spawnIndex > availableSpawnPositions.Count - 1)
+        {
+            spawnIndex = 0;
+        }
+        return newPosition;
+    }
+
+    private void SpawnPlayers()
+    {
+        foreach (PlayerInfo pi in GameData.Instance.allPlayers)
+        {
+            Player playerInst = Instantiate(playerPrefab, GetNextSpawnLocation(), Quaternion.identity);
+            playerInst.GetComponent<NetworkObject>().SpawnWithOwnership(pi.clientId);
+            playerInst.PlayerColor.Value = pi.color;
+        }
+    }
 }
